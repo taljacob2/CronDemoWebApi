d6c93d636ede1ed2d3358d9876190bb4ebc85056
Tal Jacob <taljacob2@gmail.com>
2024-10-08 23:28:01 +0300

    Concise UserTasks

--- CronDemoWebApi/Tasks/UserTasks.cs
+++ CronDemoWebApi/Tasks/UserTasks.cs
@@ -1,5 +1,4 @@
 ﻿using CronDemoWebApi.Services;
-using System.Collections.Concurrent;
 
 namespace CronDemoWebApi.Tasks
 {
@@ -19,32 +18,7 @@ namespace CronDemoWebApi.Tasks
             _logger.LogInformation("Recurring job for all users!");
             var db = new DB();
 
-            // TODO:
-            //var progress = logger.WriteProgressBar();
-            var userCount = db.Users.Count();
-            var successfulTasks = new ConcurrentBag<bool>(); // Thread-safe collection to count successful tasks
-
-            Parallel.ForEach(db.Users, user =>
-            {
-                try
-                {
-                    RunUser(user);
-                    successfulTasks.Add(true); // Add to the successful tasks
-                }
-                catch (Exception ex)
-                {
-                    // Handle or log exceptions as needed
-                    _logger.LogError(ex.ToString());
-                }
-                finally
-                {
-                    // Update the progress bar safely
-                    var completedCount = successfulTasks.Count;
-                    var progressValue = (completedCount * 100) / userCount;
-                    // TODO:
-                    //progress.SetValue(progressValue);
-                }
-            });
+            Parallel.ForEach(db.Users, RunUser);
         }
 
         private void RunUser(string user)
